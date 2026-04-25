0978a55c6d56ef6d2a24cfdcd795802d5587ec43
Samantha Roman <samantharaeroman@gmail.com>
2026-04-25 13:18:59 -0700

    Initial Commit

---
+++ MindSweep/MindSweepApp.swift
@@ -0,0 +1,17 @@
+//
+//  MindSweepApp.swift
+//  MindSweep
+//
+//  Created by Samantha Roman on 4/25/26.
+//
+
+import SwiftUI
+
+@main
+struct MindSweepApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
